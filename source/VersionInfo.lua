63599a7bf43006cd912585fc6cb797ae804d9c40
Silent <zdanio95@gmail.com>
2020-05-10 13:16:01 +0200

    Bump up version number

--- source/VersionInfo.lua
+++ source/VersionInfo.lua
@@ -1,7 +1,7 @@
 defines { 
     "rsc_FullName=\"DXHR DC - Gold Filter Restoration\"",
     "rsc_MinorVersion=0",
-    "rsc_RevisionID=1",
+    "rsc_RevisionID=3",
     "rsc_BuildID=0",
     "rsc_Copyright=\"2020\"",
     "rsc_UpdateURL=\"RELEASE URL\""
