682f40e5e3c2803421e473fe0e4d3e3d3780db8d
Silent <zdanio95@gmail.com>
2021-04-21 17:40:02 +0200

    Bump up version number

--- source/VersionInfo.lua
+++ source/VersionInfo.lua
@@ -1,8 +1,8 @@
 defines { 
     "rsc_FullName=\"DXHR DC - Gold Filter Restoration\"",
     "rsc_MinorVersion=0",
-    "rsc_RevisionID=3",
+    "rsc_RevisionID=4",
     "rsc_BuildID=0",
-    "rsc_Copyright=\"2020\"",
+    "rsc_Copyright=\"2020-2021\"",
     "rsc_UpdateURL=\"https://github.com/CookiePLMonster/DXHRDC-GFX/releases\""
 }
